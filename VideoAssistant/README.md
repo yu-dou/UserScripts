383e1e1f0de26d9461ec3a56cbad49548d600722
Aoerz <aoerz@qq.com>
2019-04-19 16:46:05 +0800

    update

---
+++ VideoAssistant/README.md
@@ -0,0 +1,35 @@
+# VideoAssistant
+
+参考 [YouTube 键盘快捷键](<https://support.google.com/youtube/answer/7631406?hl=zh-Hans>) 实现了常用的几个快捷键
+
+### 按键说明
+
+- <kbd>SPACE</kbd> 键控制播放与暂停    
+
+- <kbd>F</kbd> 键开启全屏与退出全屏   
+
+- <kbd>LEFT</kbd> 键快退10s   
+
+- <kbd>RIGHT</kbd> 键快进10s   
+
+- <kbd>UP</kbd> 键使音量放大10%   
+
+- <kbd>DOWN</kbd> 键使音量减小10%
+
+- <kbd>M</kbd> 静音
+
+- <kbd>home</kbd> 回到视频头
+
+- <kbd>end</kbd> 跳转到视频末尾
+
+  
+
+## 使用方法
+
+1. 安装一个用户脚本管理器，例如 [油猴](<https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?utm_source=chrome-ntp-icon>) 插件
+
+2. 复制 VideoAssistant.js 中的内容到油猴新建脚本或 [点击自动安装](https://greasyfork.org/scripts/381103-%E8%A7%86%E9%A2%91%E5%8A%A9%E6%89%8B/code/%E8%A7%86%E9%A2%91%E5%8A%A9%E6%89%8B.user.js)
+
+   
+
+   
---
+++ VideoAssistant/VideoAssistant.js
@@ -0,0 +1,116 @@
+﻿// ==UserScript==
+// @name         视频助手
+// @namespace    https://github.com/Aoerz/UserScripts
+// @version      0.1
+// @description  使用键盘控制视频播放
+// @author       Aoerz
+// @include      https://www.icourse163.org/*
+// @include      https://www.xuetangx.com/*
+// @include      https://www.coursera.org/*
+// @include      https://www.bilibili.com/*
+// @include      https://v.qq.com/*
+// @include      https://www.iqiyi.com/*
+// @include      https://v.youku.com/*
+// @grant        none
+// ==/UserScript==
+(function() {
+  "use strict";
+  var skipTime = 10;
+  var skipVolume = 0.1;
+  var volumeTemp;
+  function xuetangxAssistant() {
+    document.onkeydown = function(e) {
+      var evt = e || window.event;
+      var v = document.querySelector("video");
+      event.preventDefault();
+      switch (evt.keyCode) {
+        case 37: {
+          //left
+          if (v.currentTime < skipTime) {
+            v.currentTime = 0;
+            alert("已到达视频开头，无法快退");
+          }
+          console.log("快退" + skipTime + "s");
+          v.currentTime = v.currentTime - skipTime;
+          break;
+        }
+        case 39: {
+          //right
+          if (v.currentTime > v.duration - skipTime) {
+            alert("已到达视频末尾，无法快进");
+            v.currentTime = v.duration;
+          }
+          console.log("快进" + skipTime + "s");
+          v.currentTime = v.currentTime + skipTime;
+          break;
+        }
+        case 38: {
+          //up
+          if (v.volume > 1 - skipVolume) {
+            alert("到达最大音量");
+            v.volume = 1;
+            break;
+          }
+          console.log("音量放大" + skipVolume * 100 + "%");
+          v.volume = v.volume + 0.1;
+          break;
+        }
+        
+        case 40: {
+          //down
+          if (v.volume < skipVolume) {
+            alert("到达最小音量");
+            v.volume = 0;
+            break;
+          }
+          console.log("音量减小" + skipVolume * 100 + "%");
+          v.volume = v.volume - skipVolume;
+          break;
+        }
+        case 32: {
+          //pause
+          if (v.paused) {
+            console.log("播放");
+            v.play();
+          } else {
+            console.log("暂停");
+            v.pause();
+          }
+          break;
+        }
+        case 70: {
+          //fullscreen
+          if (!document.fullscreenElement) {
+            v.requestFullscreen();
+          } else {
+            if (document.exitFullscreen) {
+              document.exitFullscreen();
+            }
+          }
+          break;
+        }
+        case 77: {
+          //muse
+          if (v.volume === 0) {
+            v.volume = volumeTemp;
+          } else {
+            volumeTemp = v.volume;
+            v.volume = 0;
+          }
+          break;
+        }
+        case 36: {
+          //home
+          v.currentTime = 0;
+          break;
+        }
+        case 35: {
+          //end
+          v.currentTime = v.duration;
+          break;
+        }
+      }
+    };
+  }
+  xuetangxAssistant();
+})();
